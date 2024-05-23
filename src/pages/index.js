8c4f02c99ed5f15d7ad00723f76c0be0c6e17ed0
Sarah Van Wellen <sarahvanwellen@gmail.com>
2024-05-23 13:53:35 +0200

    bollen toegeovoegd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,33 @@
-// Home.js
 import React from 'react';
 import useEvents from '@/data/events';
 import styles from '@/styles/Home.module.css';
 
-const Home = ({ onSelectCharacter }) => {
+const Home = () => {
   const { data, isLoading, isError } = useEvents();
+
+  const onSelectEvent = (event) => {
+    alert(`Geselecteerd evenement: ${event.title}\nBeschrijving: ${event.description}`);
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error</div>;
 
+  const sortedEvents = [...data.data.results].sort((a, b) => new Date(b.date) - new Date(a.date));
+
   return (
-    <div className={styles.main}> 
-      <h1>Evenementen</h1>
-      {data.data.results.map(event => (
-        <div key={event.id} onClick={() => onSelectCharacter(event)}>
-          <h2>{event.title}</h2>
-        </div>
-      ))}
+    <div className={styles.main}>
+      <div className={styles.ballsContainer}>
+        {sortedEvents.map(event => (
+          <div key={event.id} className={styles.ball}></div>
+        ))}
+      </div>
+      <div className={styles.eventsContainer}>
+        {sortedEvents.map(event => (
+          <div key={event.id} onClick={() => onSelectEvent(event)} className={styles.event}>
+            <h2>{event.title}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
