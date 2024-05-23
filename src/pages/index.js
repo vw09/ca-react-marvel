795f517ffd6001afd7187adbbb8fc1410277c464
Sarah Van Wellen <sarahvanwellen@gmail.com>
2024-05-23 14:44:24 +0200

    extra bollen en titel toegevoegt

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,21 +12,36 @@ const Home = () => {
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error</div>;
 
+  const totalBalls = 20;
+
   const sortedEvents = [...data.data.results].sort((a, b) => new Date(b.date) - new Date(a.date));
+  const eventsToShow = sortedEvents.slice(0, totalBalls);
+
+  // Dummygegevens voor de overgebleven bollen
+  const remainingBalls = totalBalls - eventsToShow.length;
+  const dummyBalls = [...Array(remainingBalls)].map((_, index) => ({
+    id: `dummy-${index}`,
+    title: '',
+  }));
+
+  const allItems = [...eventsToShow, ...dummyBalls];
 
   return (
     <div className={styles.main}>
-      <div className={styles.ballsContainer}>
-        {sortedEvents.map(event => (
-          <div key={event.id} className={styles.ball}></div>
-        ))}
-      </div>
-      <div className={styles.eventsContainer}>
-        {sortedEvents.map(event => (
-          <div key={event.id} onClick={() => onSelectEvent(event)} className={styles.event}>
-            <h2>{event.title}</h2>
-          </div>
-        ))}
+       <h1>The Chronicles of Marvel</h1> {/* Titel */}
+      <div className={styles.container}>
+        <div className={styles.ballsContainer}>
+          {allItems.map((item, index) => (
+            <div key={item.id || index} className={styles.ball}></div>
+          ))}
+        </div>
+        <div className={styles.eventsContainer}>
+          {eventsToShow.map(event => (
+            <div key={event.id} onClick={() => onSelectEvent(event)} className={styles.event}>
+              <h2>{event.title}</h2>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
