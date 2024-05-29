48b9019f8fdc741f50ec69e43e2b6cd3c27b2362
Sarah Van Wellen <sarahvanwellen@gmail.com>
2024-05-29 12:27:19 +0200

    characters toegevoegd

--- src/pages/detail/[id].js
+++ src/pages/detail/[id].js
@@ -1,8 +1,9 @@
 import React from 'react';
-
-import styles from '@/styles/Home.module.css';
 import { useRouter } from 'next/router'
 import useEvent from '@/data/event';
+import styles from '@/styles/Detail.module.css';
+
+
 
 const Index2 = () => {
   const router = useRouter()
@@ -14,7 +15,7 @@ console.log(data)
   if (isError) return <div>Error</div>;
 
   return (
-    <div>
+    <div className={styles.main}>
       <h1>{data.data.results[0].title}</h1>
  <p>{data.data.results[0].description}</p>
   <div>{data.data.results[0].characters.items.map(char => {
