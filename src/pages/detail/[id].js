a1f02a9126154240fcdd926823ab33133ecd2b2b
Sarah Van Wellen <sarahvanwellen@gmail.com>
2024-05-29 13:35:27 +0200

    characters en description zijn onderverdeeld in secties + lettertypen vervangen

--- src/pages/detail/[id].js
+++ src/pages/detail/[id].js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import useEvent from '@/data/event';
 import styles from '@/styles/Detail.module.css';
 
-
-
 const Index2 = () => {
-  const router = useRouter()
+  const router = useRouter();
   const { data, isLoading, isError } = useEvent(router.query.id);
 
-console.log(data)
+  console.log(data);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error</div>;
@@ -20,10 +18,18 @@ console.log(data)
         Terug
       </button>
       <h1>{data.data.results[0].title}</h1>
- <p>{data.data.results[0].description}</p>
-  <div>{data.data.results[0].characters.items.map(char => {
-    return (<div key={char.resourceURI}>{char.name}</div>)
-  })}</div>
+      <div className={styles.descriptionBlock}>
+        <h2 className={styles.descriptionTitle}>Description</h2>
+        <p>{data.data.results[0].description}</p>
+      </div>
+      <div className={styles.charactersBlock}>
+        <h2 className={styles.charactersTitle}>Characters</h2>
+        <div>
+          {data.data.results[0].characters.items.map((char) => {
+            return <div key={char.resourceURI}>{char.name}</div>;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
